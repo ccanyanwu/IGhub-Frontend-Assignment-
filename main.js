e6793c418c7c67b302ac6d57c740454c6ca0a7ff
ccanyanwu <eme42c@gmail.com>
2020-08-17 15:14:15 +0100

    First javascript commit on ighub

---
+++ main.js
@@ -0,0 +1,14 @@
+//declare variables 
+var x = 5,
+    y = 6;
+    
+//print to the web page the sum of x and y
+document.write(x + y);
+
+//display the sum of the result as an alert on the webpage 
+alert(x + y);
+
+//take in 2 integers and prints alerts their sum
+ var firstInteger = prompt('What is your first integer?', 0);
+ var secondInteger= prompt('and your second integer?', 0)
+ alert(`The sum of your integers is ${Number(firstInteger) + Number(secondInteger)}`);
