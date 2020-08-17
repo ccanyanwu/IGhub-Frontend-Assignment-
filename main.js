f1924bc9c1b45b06c72a53682b700f2502ac183a
ccanyanwu <eme42c@gmail.com>
2020-08-17 18:49:59 +0100

    Styled it

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //declare variables 
 var x = 5,
-    y = 6;
-    
+    y = 6, 
+    z = x + y;
 //print to the web page the sum of x and y
-document.write(x + y);
+document.write(`The sum of 5 and 6 is ${z}`);
 
 //display the sum of the result as an alert on the webpage 
-alert(x + y);
+alert(`The sum of 5 and 6 is ${z} `);
 
 //take in 2 integers and prints alerts their sum
  var firstInteger = prompt('What is your first integer?', 0);
  var secondInteger= prompt('and your second integer?', 0)
  
  //alerts the sum of both integers 
- alert(`The sum of your integers is ${Number(firstInteger) + Number(secondInteger)}`);
+ alert(`The sum of your integers ${firstInteger} and ${secondInteger} is ${Number(firstInteger) + Number(secondInteger)}`);
