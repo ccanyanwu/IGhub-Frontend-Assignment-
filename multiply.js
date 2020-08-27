488796d7f253dfbb65261809cfc5c5d49f36c99a
ccanyanwu <eme42c@gmail.com>
2020-08-27 20:27:22 +0100

    Added styling

--- multiply.js
+++ multiply.js
@@ -2,10 +2,10 @@
 const multiplier = document.querySelector('#multiplier'),
       range = document.querySelector('#range'), 
       multiplyBtn = document.querySelector('#button'),
+      table = document.querySelector('table'),
+      thead = document.querySelector('thead'), 
       article = document.querySelector('article');
 
-
-      
 //event listener to listen for button click 
 multiplyBtn.addEventListener('click', generateTable);
 
@@ -15,18 +15,24 @@ function generateTable(){
   let UIrange =Number(range.value),
       result;
   
-  //condition to check for empty input
-  if (multiplier.value === '' || range.value === '' || Number(range.value) > 100) {
-    alert('Figures must be provided in both fields and range must not be more than 100');
+  //condition to check for empty input or exceeded limit 
+  if (multiplier.value === '' || range.value === '' || Number(range.value) > 12) {
+    alert('Figures must be provided in both fields and range must not be more than 13');
     
   } else {
     //loop through the range to generate individual result 
     for (let i = 1 ; i <= UIrange; i++) {
       result = Number(multiplier.value) * i;
-      p = document.createElement("p");
-      p.innerText = `${multiplier.value} X ${i} = ${result}` ;
-      article.appendChild(p);
+      //p = document.createElement("p");
+      //p.innerText = `${multiplier.value} X ${i} = ${result}` ;
+      //article.appendChild(p);
+      tr = document.createElement("tr");
+      tr.classList = 'roww';
+      tr.innerHTML = `<td>${multiplier.value} X ${i}</td>
+                      <td>${result}</td>`;
+      table.appendChild(tr);
       
+      console.log(tr);
     }
     //clears the input field 
     multiplier.value = '';
@@ -39,7 +45,11 @@ function generateTable(){
 
 //clear result 
 multiplier.addEventListener('keydown', function(){
-  article.innerText = '';
+  table.innerText = '';
+  table.innerHTML = `<thead>
+          <th>MULTIPLICATION </th>
+          <th>RESULT </th>
+        </thead>`
   multiplyBtn.disabled = false;
 } )
       
