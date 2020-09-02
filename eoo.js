231d47342934d4be1e0346cc2d7691a814aa4fca
ccanyanwu <eme42c@gmail.com>
2020-09-02 13:37:20 +0100

    First commit

---
+++ eoo.js
@@ -0,0 +1,21 @@
+//select elements from the DOM
+const form = document.querySelector('#form'),
+      status = document.querySelector('.status'), 
+      input = document.querySelectorAll('input'), 
+      btn = document.querySelector('#submit');
+      
+ //add a submit event listener to the dom
+ document.addEventListener('DOMContentLoaded', validateNumber);
+ 
+ //validate number function 
+ function validateNumber(){
+   let numbers = 50,
+       ol = document.querySelector('ol');
+  for (let i = 1; i <= numbers; i++){
+    if (i % 2 !== 0) {
+      ol.innerHTML += `<li>YES</li>`;
+    } else {
+      ol.innerHTML += `<li>NO</li>`;
+    }
+  } 
+} 
