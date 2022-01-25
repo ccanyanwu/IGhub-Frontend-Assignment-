0119bca5f0c99a98937524618fc7b5726fb0c843
ccanyanwu <eme42c@gmail.com>
2022-01-25 23:42:21 +0100

    done with no edge cases

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const numbers = document.querySelectorAll(".number"),
   operationDisplay = document.querySelector(".first"),
   resultDisplay = document.querySelector(".second");
 
-//clear result
-clear.addEventListener("click", () => {
-  operationDisplay.value = "";
-});
-//get operation function
-const getOperator = operator => {
+//clear display
+clear.addEventListener("click", () => (operationDisplay.value = ""));
+
+//get symbols
+const getOperator = (operator) => {
   switch (operator) {
     case "-":
       return "-";
@@ -20,17 +19,20 @@ const getOperator = operator => {
     case "+":
       return "+";
       break;
-
     case "/":
       return "/";
       break;
     case "x":
       return "*";
       break;
+    case ".":
+      return ".";
+      break;
     default:
       break;
   }
-}
+};
+
 //select numbers
 numbers.forEach((number) =>
   number.addEventListener("click", (e) => {
@@ -39,20 +41,24 @@ numbers.forEach((number) =>
 );
 
 //select decimal
-decimal.addEventListener("click", (e) => {
-  operationDisplay.value += e.target.value;
-});
+decimal.addEventListener(
+  "click",
+  (e) => (operationDisplay.value += getOperator(e.target.value))
+);
 
 //select operators
 operators.forEach((operator) =>
-  operator.addEventListener("click", (e) => {
-    const operator = getOperator(e.target.value)
-    console.log(operator);
-    operationDisplay.value += operator;
-  })
+  operator.addEventListener(
+    "click",
+    (e) => (operationDisplay.value += getOperator(e.target.value))
+  )
 );
 
-//calculate result
+//calculate & display result
 calculate.addEventListener("click", (e) => {
-  alert([...operationDisplay.value].indexOf('*'))
+  const result = eval(operationDisplay.value);
+
+  //check if result is a float or interger
+  resultDisplay.textContent =
+    Number(result) === result && result % 1 !== 0 ? result.toFixed(2) : result;
 });
