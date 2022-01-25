08d45bb1b29f3060a1f99461f12e3c02e81c039e
ccanyanwu <eme42c@gmail.com>
2022-01-25 11:02:11 +0100

    started the functionality

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
-//select items from DOM 
-const numbers = document.querySelectorAll('.number'),
-      clear = document.querySelector('.clear'), 
-      decimal = document.querySelector('#symbol'), 
-      operators = document.querySelectorAll('.operators'), 
-      calculate = document.querySelector('#result'),
-      operationDisplay = document.querySelector('.first'), 
-      resultDisplay = document.querySelector('.second');
-      
- operators.forEach(button => button.addEventListener('click', (e)=>console.log(e.target.value)));
- calculate.addEventListener('click', (e) => console.log(e.target.value))
- //operationDisplay.textContent = 5
- //resultDisplay.textContent = 5544
+//select items from DOM
+const numbers = document.querySelectorAll(".number"),
+  clear = document.querySelector(".clear"),
+  decimal = document.querySelector("#symbol"),
+  operators = document.querySelectorAll(".operators"),
+  calculate = document.querySelector("#result"),
+  operationDisplay = document.querySelector(".first"),
+  resultDisplay = document.querySelector(".second");
+
+//clear result
+clear.addEventListener("click", () => {
+  operationDisplay.value = "";
+});
+//get operation function
+const getOperator = operator => {
+  switch (operator) {
+    case "-":
+      return "-";
+      break;
+    case "+":
+      return "+";
+      break;
+
+    case "/":
+      return "/";
+      break;
+    case "x":
+      return "*";
+      break;
+    default:
+      break;
+  }
+}
+//select numbers
+numbers.forEach((number) =>
+  number.addEventListener("click", (e) => {
+    operationDisplay.value += number.value;
+  })
+);
+
+//select decimal
+decimal.addEventListener("click", (e) => {
+  operationDisplay.value += e.target.value;
+});
+
+//select operators
+operators.forEach((operator) =>
+  operator.addEventListener("click", (e) => {
+    const operator = getOperator(e.target.value)
+    console.log(operator);
+    operationDisplay.value += operator;
+  })
+);
+
+//calculate result
+calculate.addEventListener("click", (e) => {
+  alert([...operationDisplay.value].indexOf('*'))
+});
