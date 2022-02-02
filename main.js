481d3c614aae8deb3daf490a3217c4e5d553947f
ccanyanwu <eme42c@gmail.com>
2022-02-02 12:29:14 +0100

    Just a sample

---
+++ main.js
@@ -0,0 +1,6 @@
+//Click handler on the document
+document.addEventListener("click", e => { console.log("Document handler"); }); 
+// Click handler on the paragraph
+document.getElementById("para").addEventListener("click", e => { console.log("Paragraph handler"); }); 
+// Click handler on the button
+document.getElementById("propa").addEventListener("click", e => { console.log("Button handler"); });
